5acac54599e05205046cb489e50b54f9eab28ffa
memo1164 <lzy259475@gmail.com>
2024-03-28 00:21:31 +0800

    添加项目文件。

---
+++ main.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+
+#include "geometry.h"
+#include "model.h"
+#include "our_gl.h"
+#include "tgaimage.h"
+
+Model* model = nullptr;
+const int width = 800;
+const int height = 800;
+
+Vec3f camera(0, 0, 3);
+Vec3f center(0, 0, 0);
+Vec3f light_dir(1, 1, 1);
+Vec3f up(0, 1, 0);
+
+struct GouraudShader : public IShader
+{
+    mat<2, 3, float> varying_uv;    // 存储三角形每个顶点的纹理坐标
+    mat<4, 4, float> unifrom_M;     // Projection*ModelView，用于将模型世界坐标转换到屏幕空间坐标
+    mat<4, 4, float> unifrom_MIT;   // unifrom_M 的逆转置，用于变换法线方向以保证法线方向依旧垂直于变换后的表面
+
+    /*
+        vertex 顶点着色器
+        参数: 面编号，顶点编号
+        返回：Vec4f
+        功能：1.根据接受的参数从模型中读取：顶点坐标、纹理坐标，并存储uv坐标
+              2.计算并返回输入顶点经过MVP变换后的坐标
+    */
+    virtual Vec4f vertex(int nface, int nvert){
+        Vec4f gl_Vertex = embed<4>(model->vert(nface, nvert));
+        gl_Vertex = Viewport * Projection * Modelview * gl_Vertex;
+        varying_uv.set_col(nvert, model->uv(nface, nvert));
+        return gl_Vertex;
+    }
+
+    /*
+        fragment 片段着色器
+        参数：重心坐标，颜色
+        返回：bool
+        功能：纹理坐标插值、法线坐标系变换 & 光源坐标系变换、简化的phong模型
+             最终经过以上计算得到颜色值，返回 bool 值标志该片段是否丢弃
+    */
+    virtual bool fragment(Vec3f bc, TGAColor &color){
+        Vec2f uv = varying_uv * bc;
+        Vec3f normal = proj<3>(unifrom_MIT * embed<4>(model->normal(uv))).normalize();
+        Vec3f light = proj<3>(unifrom_M * embed<4>(light_dir)).normalize();
+        Vec3f eye = proj<3>(unifrom_M * embed<4>(camera)).normalize();
+        float diff = std::max(0.f, normal * light);
+        // 半程向量与法线的夹角
+        float spec = std::pow(std::max(0.f, (light + eye).normalize() * normal), model->specular(uv));
+        TGAColor c = model->diffuse(uv);
+        for (int i = 0; i < 3; i++) {
+            color[i] = std::min((double)255.f, 5 + c[i] * (diff + 0.5 * spec));
+        }
+        return false;
+    }
+};
+
+int main(int argc, char** argv) {
+    if (2==argc) {
+        model = new Model(argv[1]);
+    } else {
+        model = new Model("G:/memo/Code/tinyrenderer/Lesson_6_Shaders_for_the_software_renderer/obj/diablo3_pose.obj");
+    }
+
+    modelview(camera, center, up);
+    viewport(width/8, height/8, width*3/4, height*3/4);
+    projection(-1.f/(camera-center).norm());
+    light_dir.normalize();
+
+    TGAImage image  (width, height, TGAImage::RGB);
+    TGAImage zbuffer(width, height, TGAImage::GRAYSCALE);
+
+    GouraudShader shader;
+    shader.unifrom_M = Projection * Modelview;
+    shader.unifrom_MIT = shader.unifrom_M.invert_transpose();
+
+    for (int i=0; i<model->nfaces(); i++) {
+        Vec4f screen_coords[3];
+        for (int j=0; j<3; j++) {
+            screen_coords[j] = shader.vertex(i, j);
+        }
+        triangle(screen_coords, shader, image, zbuffer);
+    }
+
+    image.  flip_vertically(); // to place the origin in the bottom left corner of the image
+    zbuffer.flip_vertically();
+    image.  write_tga_file("output.tga");
+    zbuffer.write_tga_file("zbuffer.tga");
+
+    delete model;
+    return 0;
+}
---
+++ our_gl.cpp
@@ -0,0 +1,126 @@
+#include <cmath>
+#include <limits>
+#include <cstdlib>
+#include "our_gl.h"
+
+Matrix Modelview;
+Matrix Viewport;
+Matrix Projection;
+
+IShader::~IShader() {}
+
+/*
+    viewport
+    参数：视口起始x坐标，视口起始y坐标，视口宽度，视口高度
+    返回：无
+    功能：计算视口变换矩阵
+*/
+void viewport(int x, int y, int w, int h){
+    Viewport = Matrix::identity();
+    Viewport[0][3] = x + w / 2.f;
+    Viewport[1][3] = y + h / 2.f;
+    Viewport[2][3] = 255.f / 2.f;
+    Viewport[0][0] = w / 2.f;
+    Viewport[1][1] = h / 2.f;
+    Viewport[2][2] = 255.f / 2.f;
+}
+
+/*
+    projection
+    参数：系数（-1/c）
+    返回：无
+    功能：计算透视变换矩阵
+*/
+void projection(float coeff){
+    Projection = Matrix::identity();
+    Projection[3][2] = coeff;
+}
+
+/*
+    modelview
+    参数：相机坐标、相机观察的中心点坐标、指向原坐标系上方的方向向量up(0,1,0)
+    返回：无
+    功能：计算模型变换矩阵
+*/
+void modelview(Vec3f camera, Vec3f center, Vec3f up){
+    //计算出z，根据z和up算出x，再算出y
+    Vec3f z = (camera - center).normalize();
+    Vec3f x = cross(up, z).normalize();
+    Vec3f y = cross(z, x).normalize();
+    Modelview = Matrix::identity();
+    for (int i = 0; i < 3; i++){
+        Modelview[0][i] = x[i];
+        Modelview[1][i] = y[i];
+        Modelview[2][i] = z[i];
+        Modelview[i][3] = -center[i];
+    }
+}
+
+// 两种不同的重心坐标求解方式，可以参考 https://zhuanlan.zhihu.com/p/144360079
+/*
+    barycentric
+    参数：顶点坐标1，顶点坐标2，顶点坐标3，坐标P
+    返回：Vec3f
+    功能：返回点P在由三个顶点坐标围成的三角形中的重心坐标
+*/
+Vec3f barycentric(Vec2f A, Vec2f B, Vec2f C, Vec2f P) {
+    Vec3f s[2];
+    for (int i=2; i--; ) {
+        s[i][0] = C[i]-A[i];
+        s[i][1] = B[i]-A[i];
+        s[i][2] = A[i]-P[i];
+    }
+    Vec3f u = cross(s[0], s[1]);
+    if (std::abs(u[2])>1e-2) // dont forget that u[2] is integer. If it is zero then triangle ABC is degenerate
+        return Vec3f(1.f-(u.x+u.y)/u.z, u.y/u.z, u.x/u.z);
+    return Vec3f(-1,1,1); // in this case generate negative coordinates, it will be thrown away by the rasterizator
+}
+
+Vec3f barycentric_(Vec2f A, Vec2f B, Vec2f C, Vec2f P) {
+    float alpha = (-1 * (P.x - B.x) * (C.y - B.y) + (P.y - B.y) * (C.x - B.x)) / (-1 * (A.x - B.x) * (C.y - B.y) + (A.y - B.y) * (C.x - B.x));
+    float beta = (-1 * (P.x - C.x) * (A.y - C.y) + (P.y - C.y) * (A.x - C.x)) / (-1 * (B.x - C.x) * (A.y - C.y) + (B.y - C.y) * (A.x - C.x));
+    float gamma = 1.f - alpha - beta;
+    if(alpha && beta && gamma) return Vec3f(alpha, beta, gamma);
+    return Vec3f(-1, 1, 1);
+}
+
+/*
+    triangle
+    参数：顶点坐标数组，着色器，目标图像，深度缓冲区
+    返回：无
+    功能：光栅化器，将模型中每个三角形，使用指定的着色器来渲染这个三角形到目标图像上，同时利用深度缓冲区来正确处理遮挡关系。
+          主函数的内循环完成时：
+            - 光栅化器得到：三角形三个顶点经过MVP变换后的坐标（屏幕世界坐标）；
+            - 着色器得到：存储该三角形每个顶点的纹理坐标的矩阵；
+          光栅化器遍历位于该三角形内的所有片段，计算其重心坐标并将其传递给片段着色器，再将片段着色器返回的颜色值写入图像
+*/
+void triangle(Vec4f *pts, IShader &shader, TGAImage &image, TGAImage &zbuffer){
+    // boundingbox
+    Vec2f boundingbox_max(std::numeric_limits<float>::min(), std::numeric_limits<float>::min());
+    Vec2f boundingbox_min(std::numeric_limits<float>::max(), std::numeric_limits<float>::max());
+    Vec2i P;    //pixel
+    TGAColor color;
+    for(int i = 0; i < 3; i++){
+        for(int j = 0; j < 2; j++){
+            boundingbox_max[j] = std::max(pts[i][j] / pts[i][3], boundingbox_max[j]);
+            boundingbox_min[j] = std::min(pts[i][j] / pts[i][3], boundingbox_min[j]);
+        }
+    }
+
+    for(P.x = boundingbox_min.x; P.x <= boundingbox_max.x; P.x++){
+        for(P.y = boundingbox_min.y; P.y <= boundingbox_max.y; P.y++){
+            // 计算重心坐标 (参数为原坐标)
+            Vec3f bc = barycentric_(proj<2>(pts[0] / pts[0][3]), proj<2>(pts[1] / pts[1][3]), proj<2>(pts[2] / pts[2][3]), proj<2>(P));
+            float P_z = (pts[0][2] / pts[0][3]) * bc.x + (pts[1][2] / pts[1][3]) * bc.y + (pts[2][2] / pts[2][3]) * bc.z;
+            int depth = std::max(0, std::min(255, (int)std::ceil(P_z)));
+            // 重心坐标有负值，说明点不在三角形上 或 没有通过深度测试 跳过
+            if (bc.x<0 || bc.y<0 || bc.z<0 || zbuffer.get(P.x, P.y)[0] > depth) continue;
+            bool discard = shader.fragment(bc, color);  // 调用片段着色器对当前像素着色
+            if(!discard){
+                // 更新
+                zbuffer.set(P.x, P.y, depth);
+                image.set(P.x, P.y, color);
+            }
+        }
+    }
+}
